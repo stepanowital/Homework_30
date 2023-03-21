960a3665452b8705da0ae98a93fb861b17e47d09
Stepanowital <stepanowital@gmail.com>
2023-03-21 15:51:42 +0600

    init commit

--- homework_28/asgi.py
+++ homework_28/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for homework_27 project.
+ASGI config for homework_28 project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'homework_27.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'homework_28.settings')
 
 application = get_asgi_application()
--- homework_28/urls.py
+++ homework_28/urls.py
@@ -1,4 +1,4 @@
-"""homework_27 URL Configuration
+"""homework_28 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
